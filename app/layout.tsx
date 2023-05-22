34dbfceb5077876832c40c4005891c28b3f40e01
Hiroki Chiyoda <contact@arafipro.com>
2023-05-22 22:20:12 +0900

    ジャンルデータを取得
    楽天市場ジャンル検索APIからデータを取得

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,7 @@ export default function RootLayout({
   return (
     <html lang="ja">
       <body className="font-sans antialiased text-gray-900 h-screen w-full flex flex-col justify-between text-center">
+        <h1>Rakuten Rankings</h1>
         {children}
         <Credit />
       </body>
